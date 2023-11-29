a98315707bc393ab7dea28c062a2267fd3ef0295
saurav kumar <saurav.kumar@urv.cat>
2023-11-29 05:17:25 +0100

    footer and examples updated

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -4,10 +4,18 @@ function Footer() {
 
 
   return (
-    <footer className="bg-body-tertiary text-black text-center py-3 foot">
+    <footer className="bg-body-tertiary text-black text-center py-1 foot">
         <div className="container">
-        <p>&copy; IISPV. All rights reserved.</p>
-        <p>🚀👨‍💻 Developed by <b>saurav kumar</b> </p>
+        
+        <span style={{justifyContent:"center",textAlign:"center"}}>
+
+        AOP-WIKI Explorer🔍 is developed by <a href="https://www.tecnatox.cat/">TecnaTox Lab</a> and <a href="https://www.iispv.cat/">Institut d'Investigació Sanitària Pere Virgili (IISPV)</a>, which is part of <a href="https://www.urv.cat/en/">Universitat Rovira I virgili</a>. AOP-WIKI Explorer development was supported by project <a href="https://www.eu-parc.eu/">Partnership for the Assessment Of risk from Chemicals (PARC)</a> funded by the European Union's Horizon Europe research and innovation Programme under <b>Grant Agreement No. 101057014</b>. Preprint available at <a href="https://www.biorxiv.org/content/10.1101/2023.11.21.568076v1">BioRxiv</a>. 
+        <p>Point of contact: saurav kumar (saurav.kumar[AT]iispv.cat)</p>
+        <p>&copy; <b>IISPV. All rights reserved.</b></p>
+
+        </span>
+
+        
         </div>
     </footer>
 );
